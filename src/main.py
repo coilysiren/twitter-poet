202decf4d85de7731742e5e6af1914f3e0cfb6e9
Lynn Cyrin <lynncyrin@gmail.com>
2018-06-06 22:32:58 -0700

    checkin auth storage

--- src/main.py
+++ src/main.py
@@ -2,38 +2,67 @@ import tweepy
 import os
 from dotenv import load_dotenv, find_dotenv
 from copy import copy
+from pymongo import MongoClient
+
+
+load_dotenv(find_dotenv())
 
 
 class TwitterService(object):
 
     def __init__(self):
-        load_dotenv(find_dotenv())
-        self.auth = tweepy.OAuthHandler(
+        self.db = DatabaseSevice()
+        self.auth = self.create_auth()
+
+    def create_auth(self):
+        return tweepy.OAuthHandler(
             os.environ['CONSUMER_KEY'],
             os.environ['CONSUMER_SECRET'],
             os.environ.get('CALLBACK_URL', 'http://localhost:5000/callback'),
         )
-        self.users = {}  # TODO: mongodb
 
-    def create_user(self, verifier, token):
-        print(token)
-        user_twitter_auth = copy(self.auth)
-        self.auth.set_access_token = (
-            token['oauth_token'],
-            token['oauth_token_secret'],
+    def create_user(self, oauth_token, oauth_verifier):
+        auth = self.create_auth()
+        auth.request_token = {
+            'oauth_token': oauth_token,
+            'oauth_token_secret': oauth_verifier,
+        }
+        auth.get_access_token(oauth_verifier)
+        self.db.create_user(
+            oauth_token=oauth_token,
+            access_token=auth.access_token,
+            access_token_secret=auth.access_token_secret,
         )
-        user_twitter_auth.get_access_token(verifier)
-        self.users[token['oauth_token']] = UserService(user_twitter_auth)
 
-    def get_user(self, token):
-        return self.users[token['oauth_token']]
+    def get_user(self, oauth_token):
+        user_data = self.db.get_user(
+            oauth_token=oauth_token,
+        )
+        auth = self.create_auth()
+        auth.set_access_token = (
+            user_data['access_token'],
+            user_data['access_token_secret'],
+        )
+        return UserService(auth)
+
+
+class DatabaseSevice(object):
+
+    def __init__(self):
+        client = MongoClient(os.environ['MONGODB_URI'])
+        self.user_collection = client.get_default_database()['users']
+
+    def create_user(self, **kwargs):
+        return self.user_collection.insert_one(kwargs)
+
+    def get_user(self, **kwargs):
+        return self.user_collection.find_one(kwargs)
 
 
 class UserService(object):
 
-    def __init__(self, user_twitter_auth):
-        self.api = tweepy.API(user_twitter_auth)
+    def __init__(self, auth):
+        self.api = tweepy.API(auth)
 
-
-def test_init():
-    assert TwitterService().auth
+    def me(self):
+        return self.api.me()
--- src/server.py
+++ src/server.py
@@ -6,7 +6,7 @@ import os
 
 load_dotenv(find_dotenv())
 app = Flask(__name__)
-app.secret_key = os.environ['CONSUMER_KEY']
+app.secret_key = os.environ['SECRET_KEY']
 twitter = TwitterService()
 
 
@@ -17,21 +17,27 @@ def index():
 
 @app.route("/start")
 def start():
-    redirect_url = twitter.auth.get_authorization_url()
-    session['request_token'] = twitter.auth.request_token
-    return redirect(redirect_url)
+    if session.get('request_token'):
+        return redirect(url_for('results'))
+    else:
+        redirect_url = twitter.auth.get_authorization_url()
+        session['request_token'] = twitter.auth.request_token
+        return redirect(redirect_url)
 
 
 @app.route("/callback")
 def callback():
     twitter.create_user(
-        verifier=request.args.get('oauth_verifier'),
-        token=session['request_token'],
+        oauth_token=request.args.get('oauth_token'),
+        oauth_verifier=request.args.get('oauth_verifier'),
     )
     return redirect(url_for('results'))
 
 
 @app.route("/results")
 def results():
-    user = twitter.get_user(session['request_token'])
-    return render_template('result.html', content=user.api.user_timeline())
+    if session.get('request_token'):
+        user = twitter.get_user(session['request_token']['oauth_token'])
+        return render_template('result.html', content=user.me())
+    else:
+        return redirect(url_for('start'))
